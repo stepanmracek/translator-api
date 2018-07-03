6df7d9c2a319676a0d0759716b5edcee9f268fbb
Stepan Mracek <stepan.mracek@gmail.com>
2018-07-03 22:45:14 +0200

    enable CORS

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 var router = express.Router();
 router.route('/:env').get((req, res) => {
     const env = req.params.env;
